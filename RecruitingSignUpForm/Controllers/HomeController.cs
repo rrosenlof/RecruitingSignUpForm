ca746f98d27d1fbad3c33c8385afc9e9b3bfce83
rrosenlof <rosenlof.ross@gmail.com>
2018-10-23 09:56:05 -0600

    adding all files

---
+++ RecruitingSignUpForm/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿/*Ross Rosenlof Oct 23, 2018*/
+
+using RoseLeaf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoseLeaf_Recruiting.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<Student> listStudents = new List<Student>();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SignUpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult SignUpForm(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                listStudents.Add(student);
+                return View("ThankYou", student);
+            }
+            else
+            {
+                //Validation Error
+                return View();
+            }
+        }
+
+        public ActionResult Display()
+        {
+            ViewBag.Results = listStudents;
+            return View();
+        }
+    }
+}
---
+++ RecruitingSignUpForm/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿/*Ross Rosenlof Oct 23, 2018*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoseLeaf.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Major { get; set; }
+        public bool EmailList { get; set; }
+        public string School { get; set; }
+        public string FieldOfInterest { get; set; }
+    }
+}
